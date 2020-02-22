7f329e0ec915732343be5c25d84b4fde1e751c3e
Hunter Perrin <hperrin@gmail.com>
2020-02-21 20:49:27 -0800

    Home page (with warning).

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,10 +2,18 @@
   <title>Sound Rodeo</title>
 </svelte:head>
 
-<h1>Great success!</h1>
+<section>
+  <h1>Sound Rodeo!!</h1>
 
-<p>
-  <strong>
-    Try editing this file (src/routes/index.svelte) to test live reloading.
-  </strong>
-</p>
+  <p>
+    This will be a soundboard app soon. Right now it's a useless app that will
+    corrupt anything you put in it. If you try to use it at all right now,
+    everything you put in it will be deleted once I update it.
+  </p>
+</section>
+
+<style>
+  section {
+    padding: 0 2em 2em;
+  }
+</style>
